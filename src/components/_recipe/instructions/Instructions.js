202d15dced1e2d6db45a0052e1e60a86d1abfbb2
Teagan Atwater <tatwater@outlook.com>
2018-09-14 22:03:31 -0400

    Fix RecipeNotes missing import

--- src/components/_recipe/instructions/Instructions.js
+++ src/components/_recipe/instructions/Instructions.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// import RecipeNotes from '../notes/Notes';
+import RecipeNotes from '../notes/Notes';
 import * as SC from './instructions_styles';
 
 
